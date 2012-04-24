5276e85d78779b55700572f4a737408716aa1d5a
J.W. Groenenberg <jw@pomtiedom.com>
2012-04-24 21:36:48 +0200

    Initial Web Api

---
+++ Atos.HTML5.Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Atos.HTML5.Demo.WebApiApplication" Language="C#" %>
---
+++ Atos.HTML5.Demo/Models/Apple.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Atos.HTML5.Demo.Models
+{
+    public class Apple
+    {
+    }
+}
