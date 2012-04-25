ce3fe8b9892a4692eed68683126c73808bd9af1f
J.W. Groenenberg <jw@pomtiedom.com>
2012-04-25 20:46:50 +0200

    start over from scratch for real this time

--- Atos.HTML5.Demo/Properties/AssemblyInfo.cs
+++ Atos.HTML5.Demo/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("deb69b70-ed82-4dd6-9e32-545eb4fa65a6")]
+[assembly: Guid("beadcdd9-6b14-4628-b9e2-98971449d1c9")]
 
 // Version information for an assembly consists of the following four values:
 //
